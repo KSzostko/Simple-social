3fbec309b373c473c93e725a66f35d5e8ea50961
Kuba Szóstko <kubaszostko@gmail.com>
2020-06-18 16:27:14 +0200

    created Button component

---
+++ src/components/Button.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import styled from 'styled-components';
+
+const StyledButton = styled.button`
+    background-color: #e4bb7a;
+    padding: 15px 40px;
+    font-size: 1.25rem;
+    color: #fff;
+    border-style: none;
+    cursor: pointer;
+    transition: 0.25s;
+
+    &:hover {
+        background-color: rgba(187, 144, 75, 0.9);
+    }
+`;
+
+const Button = ({ children }) => (
+    <StyledButton>{children}</StyledButton>
+);
+
+export default Button;
