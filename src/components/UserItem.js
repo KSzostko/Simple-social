e27c3c2477e537cd33fc16563e05f232433aae93
Kuba Szóstko <kubaszostko@gmail.com>
2020-06-20 15:10:55 +0200

    styled UserItem

--- src/components/UserItem.js
+++ src/components/UserItem.js
@@ -1,24 +1,40 @@
 import React from 'react';
+import styled from 'styled-components';
 import Button from './Button';
+
+const StyledListItem = styled.li`
+    padding: 20px;
+    border: 1px solid #000;
+`;
+
+const StyledInfoList = styled.ul`
+    display: flex;
+    flex-direction: column;
+    list-style: none;
+    padding: 0;
+    margin-bottom: 20px;
+    font-size: ${props => props.contact ? '12px' : '16px'};
+    font-weight: ${props => props.contact ? 300 : 400};
+`;
 
 const UserItem = ({ data }) => {
     const { name, email, phone, website, company } = data;
     
     return (
-        <li>
+        <StyledListItem>
             <h2>{name}</h2>
-            <div>
-                <span>{email}</span>
-                <span>{phone}</span>
-                <span>{website}</span>
-            </div>
-            <div>
-                <span>{company.name}</span>
-                <span>{company.catchPhrase}</span>
-                <span>{company.bs}</span>
-            </div>
+            <StyledInfoList contact>
+                <li>{email}</li>
+                <li>{phone}</li>
+                <li>{website}</li>
+            </StyledInfoList>
+            <StyledInfoList>
+                <li>{company.name}</li>
+                <li>{company.catchPhrase}</li>
+                <li>{company.bs}</li>
+            </StyledInfoList>
             <Button>Details</Button>
-        </li>
+        </StyledListItem>
     );
 };
 
