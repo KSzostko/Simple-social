4256d286b4ecb141cf9a662a5dfb5205ad4608a3
Kuba Szóstko <kubaszostko@gmail.com>
2020-06-23 10:05:25 +0200

    changed Button styles

--- src/components/Button.js
+++ src/components/Button.js
@@ -1,23 +1,27 @@
 import React from 'react';
-import styled from 'styled-components';
+import styled, { css } from 'styled-components';
 
-// TODO: change styles
 const StyledButton = styled.button`
     background-color: #e4bb7a;
-    padding: 15px 40px;
-    font-size: 1.25rem;
+    padding: ${props => props.round ? '0' : '7px 20px'};
+    font-size: ${props => props.round ? '3rem' : '1rem'};
+    font-weight: ${props => props.round ? 700 : 400};
     color: #fff;
-    border-style: none;
+    border: none;
     cursor: pointer;
     transition: 0.25s;
+    border-radius: ${props => props.round ? '50%' : 0};
+    ${props => props.round && css`
+        width: 50px;
+        height: 50px;
+        display: flex;
+        justify-content: center;
+        align-items: center;
+    `}
 
     &:hover {
         background-color: rgba(187, 144, 75, 0.9);
     }
 `;
 
-const Button = ({ children }) => (
-    <StyledButton>{children}</StyledButton>
-);
-
-export default Button;
+export default StyledButton;
--- src/views/UserDetails.js
+++ src/views/UserDetails.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { useParams } from 'react-router-dom';
 import PostsList from '../components/PostsList';
+import Button from '../components/Button';
 
 
 function UserDetails(props) {
@@ -9,7 +10,11 @@ function UserDetails(props) {
     
     return (
         <div>
-            hello {name}
+            <div>
+                <Button>Back</Button>
+                <h2>{name}</h2>
+                <Button round>+</Button>
+            </div>
             <PostsList userId={id} />
         </div>
     );
