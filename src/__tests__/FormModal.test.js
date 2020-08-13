0e7d543561ab68f9f95c6b476e1d1d1afb617da6
Kuba Szóstko <kubaszostko@gmail.com>
2020-08-13 09:14:36 +0200

    adjusted tests to use screen

--- src/__tests__/FormModal.test.js
+++ src/__tests__/FormModal.test.js
@@ -1,8 +1,7 @@
 import React from 'react';
 import Modal from 'react-modal';
 import { Provider } from 'react-redux';
-import '@testing-library/jest-dom/extend-expect';
-import { render } from '@testing-library/react';
+import { render, screen } from '@testing-library/react';
 import store from '../redux/store';
 import FormModal from '../components/FormModal';
 
@@ -16,35 +15,35 @@ const fakeData = {
 describe('<FormModal />', () => {
     it('renders post form if prop type=post', () => {
         Modal.setAppElement(document.createElement('div'));
-        const { getByText, getByLabelText, queryByLabelText, queryByText } = render(
+        render(
             <Provider store={store}>
                 <FormModal {...fakeData} type="post" />
             </Provider>
         );
 
-        getByText(/add post/i);
-        getByLabelText(/title/i);
-        getByLabelText(/body/i);
+        screen.getByText(/add post/i);
+        screen.getByLabelText(/title/i);
+        screen.getByLabelText(/body/i);
 
-        expect(queryByText(/add comment/i)).not.toBeInTheDocument();
-        expect(queryByLabelText(/name/i)).not.toBeInTheDocument();
-        expect(queryByLabelText(/email/i)).not.toBeInTheDocument();
+        expect(screen.queryByText(/add comment/i)).not.toBeInTheDocument();
+        expect(screen.queryByLabelText(/name/i)).not.toBeInTheDocument();
+        expect(screen.queryByLabelText(/email/i)).not.toBeInTheDocument();
     });
 
     it('renders comment form if prop type=comment', () => {
         Modal.setAppElement(document.createElement('div'));
-        const { getByText, getByLabelText, queryByText, queryByLabelText } = render(
+        render(
             <Provider store={store}>
                 <FormModal {...fakeData} type="comment" />
             </Provider>
         );
 
-        getByText(/add comment/i);
-        getByLabelText(/name/i);
-        getByLabelText(/email/i);
-        getByLabelText(/body/i);
+        screen.getByText(/add comment/i);
+        screen.getByLabelText(/name/i);
+        screen.getByLabelText(/email/i);
+        screen.getByLabelText(/body/i);
 
-        expect(queryByText(/add post/i)).not.toBeInTheDocument();
-        expect(queryByLabelText(/title/i)).not.toBeInTheDocument();
+        expect(screen.queryByText(/add post/i)).not.toBeInTheDocument();
+        expect(screen.queryByLabelText(/title/i)).not.toBeInTheDocument();
     });
 });
--- src/__tests__/PostBody.test.js
+++ src/__tests__/PostBody.test.js
@@ -1,14 +1,14 @@
 import React from 'react';
 import '@testing-library/jest-dom/extend-expect';
-import { render } from '@testing-library/react';
+import { render, screen } from '@testing-library/react';
 import { fakePost } from '../lib/testUtils';
 import PostBody from '../components/PostBody';
 
 describe('<PostBody />', () => {
     it('renders component correctly', () => {
-        const { getByText } = render(<PostBody {...fakePost()} />);
+        render(<PostBody {...fakePost()} />);
 
-        expect(getByText(/Post title/)).toBeInTheDocument();
-        expect(getByText(/Post body/)).toBeInTheDocument();
+        expect(screen.getByText(/Post title/)).toBeInTheDocument();
+        expect(screen.getByText(/Post body/)).toBeInTheDocument();
     });
 });
