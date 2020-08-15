3df740f920bfc00958477b1e55e5d01156c74199
Kuba Szóstko <kubaszostko@gmail.com>
2020-08-15 10:19:39 +0200

    added test for PostForm which checks errors display

--- src/__tests__/PostForm.test.js
+++ src/__tests__/PostForm.test.js
@@ -1,9 +1,12 @@
 import React from 'react';
-import { render, screen, fireEvent } from '@testing-library/react';
+import { render, screen, fireEvent, wait } from '@testing-library/react';
+import mockAxios from 'axios';
 import { Provider } from 'react-redux';
 import store from '../redux/store';
 import { fakePost } from '../lib/testUtils';
 import PostForm from '../components/PostForm';
+
+jest.mock('axios');
 
 const fakeData = {
     closeModal: () => {},
@@ -19,5 +22,26 @@ describe('<PostForm />', () => {
         );
 
         expect(container.firstChild).toMatchSnapshot();
+    });
+
+    it('will not submit form if it is not fully filled out, it displays errors instead and disables the submit button', async () => {
+        const fakePostData = fakePost();
+        
+        render(
+            <Provider store={store}>
+                <PostForm {...fakeData} />
+            </Provider>
+        );
+
+        await wait(() => fireEvent.click(screen.getByText(/save/i)));
+        expect(screen.getByText(/save/i)).toBeDisabled();
+
+        await wait(() => fireEvent.change(screen.getByLabelText(/title/i), {
+            target: {
+                value: fakePostData.title,
+            }
+        }));
+        expect(screen.getByText(/required/i)).toBeInTheDocument();
+        expect(mockAxios).not.toHaveBeenCalled();
     });
 });
